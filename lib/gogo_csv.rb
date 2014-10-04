a240cc96ad72cd5d5b9176572691ae2cf62f889d
gogotanaka <mail@tanakakazuki.com>
2014-10-04 11:56:17 -0700

    Init

---
+++ lib/gogo_csv.rb
@@ -0,0 +1,5 @@
+require "gogo_csv/version"
+
+module GogoCsv
+  # Your code goes here...
+end
---
+++ spec/gogo_csv_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe GogoCsv do
+  it 'has a version number' do
+    expect(GogoCsv::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
