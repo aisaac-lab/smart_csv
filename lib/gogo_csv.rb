22ae48a5861b91bf1b3fd1bed7cf705cfce1c237
gogotanaka <mail@tanakakazuki.com>
2014-10-05 16:08:56 -0700

    Update interface

--- lib/gogo_csv.rb
+++ lib/gogo_csv.rb
@@ -1,26 +1,23 @@
 require "gogo_csv/version"
+require "gogo_csv/matrix"
+require "gogo_csv/array"
 
 module GogoCsv
   require 'csv'
+  require 'matrix'
 
-  def read(path)
-    CSV.read(
+  def open(path, format=:matrix)
+    $path = path
+    arys = CSV.read(
       File.expand_path(path)
     )
-  end
 
-  class ::Array
-    def save!(path)
-      raise 'should be array of array' unless arys?
-      CSV.open(File.expand_path(path), "wb") do |csv|
-        each { |ary| csv <<  ary }
-      end
+    case format
+    when :matrix
+      Matrix[*arys]
+    when :arys
+      arys
     end
-
-    private
-    
-      def arys?
-        all? { |item| item.is_a?(Array) }
-      end
   end
+  module_function :open
 end
